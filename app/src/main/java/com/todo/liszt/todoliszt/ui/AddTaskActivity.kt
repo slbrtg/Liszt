2e49e9659c369803a4e1cc47501b0d914617a249
saul <slbrtg@gmail.com>
2017-10-04 13:53:31 -0700

    begin filling out AddTaskActivity.kt

--- app/src/main/java/com/todo/liszt/todoliszt/ui/AddTaskActivity.kt
+++ app/src/main/java/com/todo/liszt/todoliszt/ui/AddTaskActivity.kt
@@ -2,12 +2,27 @@ package com.todo.liszt.todoliszt.ui
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
 import com.todo.liszt.todoliszt.R
 
-class AddTaskActivity : AppCompatActivity() {
+
+class AddTaskActivity : AppCompatActivity(), View.OnClickListener {
+    internal lateinit var mInputTaskNameEditText: EditText
+    internal lateinit var mInputTaskDescriptionEditText: EditText
+    internal lateinit var mAddNewTaskButton: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_task)
+
+        mInputTaskNameEditText = findViewById(R.id.inputTaskNameEditText)
+        mInputTaskDescriptionEditText = findViewById(R.id.inputTaskDescriptionEditText)
+        mAddNewTaskButton = findViewById(R.id.addNewTaskButton)
+    }
+
+    override fun onClick(v: View?) {
+
     }
 }
